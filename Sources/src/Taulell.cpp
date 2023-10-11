45958a24b5dc83c6b26235e53672c4e06274198d
aRetr0 <119658916+aRetr0@users.noreply.github.com>
2023-10-11 20:39:52 +0200

    taulell.cpp per completar

--- Sources/src/Taulell.cpp
+++ Sources/src/Taulell.cpp
@@ -1,1 +1,47 @@
 #include "Taulell.h"
+
+Taulell::Taulell(){
+    nBonificacions = 0;
+    aSnake = nullptr;
+
+    for (int i = 0; i < files; i++){
+        for (int j = 0; j < columnes; j++){
+            contenidor[i][j] = 0;
+        }
+    }
+}
+
+Taulell::~Taulell(){
+    if (aSnake != nullptr){
+        delete aSnake;
+        aSnake = nullptr;
+    }
+}
+
+int Taulell::getFiles(){
+    return files;
+}
+
+int Taulell::getColumnes(){
+    return columnes;
+}
+
+int Taulell::getnBonificacions(){
+    return nBonificacions;
+}
+
+void Taulell::inici(int quantes){
+
+}
+
+void Taulell::visualitzar(){
+
+}
+
+bool Taulell::movimentSerp(MyEnum::eDirection dir){
+
+}
+
+void Taulell::finalitzar(){
+
+}
