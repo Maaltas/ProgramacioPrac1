20b91cbffc84134454d2951bd510b55404535ee6
Maltas <107067687+Maaltas@users.noreply.github.com>
2023-10-11 19:41:42 +0200

    Update Snake.cpp

--- Sources/src/Snake.cpp
+++ Sources/src/Snake.cpp
@@ -17,20 +17,26 @@ Snake::Snake(int nBonificacions) {
     currentDir = MyEnum::Rigth;
 }
 bool Snake::visualitza(int row, int col) {
+    Position *temp = new Position(row,col);
     for (int i = 0; i < currentLength; i++) {
-        if (arrayPosition[i] == Position(row, col)) {
+        if (arrayPosition[i] == temp) {
             arrayPosition[i].visualitza();
+            delete temp;
             return true;
         }
     }
+    delete temp;
     return false;
 }
 bool Snake::contains(int row, int col) {
+    Position *temp = new Position(row,col);
     for (int i = 0; i < currentLength; i++) {
-        if (arrayPosition[i] == Position(row, col)) {
+        if (arrayPosition[i] == temp) {
+            delete temp;
             return true;
         }
     }
+    delete temp;
     return false;
 }
 
